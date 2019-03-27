cd01f18c8695d5c3bdf202214889d38473238610
nightraindust <seankoke@gmail.com>
2019-03-26 20:52:10 -0500

    #176- add iCal link

--- chipy_org/apps/meetings/feeds.py
+++ chipy_org/apps/meetings/feeds.py
@@ -2,11 +2,50 @@ from datetime import timedelta
 from django_ical.views import ICalFeed
 from .models import Meeting
 
+class MeetingFeed(ICalFeed): 
+     
+    product_id = '-//chipy.org//Meeting//EN'
+    timezone = 'CST'
+
+    def items(self):
+        return Meeting.objects.order_by('-when')[0]
+
+    def item_description(self, item):
+        description = 'RSVP at http://chipy.org\n\n'
+        for topic in item.topics.all():
+            presenter_name = 'None Given'
+            if topic.presentors.count() > 0:
+                presenter_name = topic.presentors.all()[0].name
+
+            description += u'{title} by {speaker}\n{description}\n\n'.format(
+                title=topic.title,
+                speaker=presenter_name,
+                description=topic.description)
+        return description
+
+    def item_link(self, item):
+        return ''
+
+    def item_location(self, item):
+        if item.where:
+            return item.where.address
+        else:
+            return 'To be determined...'
+
+    def item_start_datetime(self, item):
+        return item.when
+
+    def item_end_datetime(self, item):
+        return item.when + timedelta(hours=1)
+
+    def item_title(self, item):
+        return 'ChiPy Meeting'
+
+
+"""
 
 class MeetingFeed(ICalFeed):
-    """
-    A iCal feed for meetings
-    """
+    
     product_id = '-//chipy.org//Meeting//EN'
     timezone = 'CST'
 
@@ -43,3 +82,4 @@ class MeetingFeed(ICalFeed):
 
     def item_title(self, item):
         return 'ChiPy Meeting'
+"""
